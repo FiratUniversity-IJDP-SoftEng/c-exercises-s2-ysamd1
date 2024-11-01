05453034eada422677970763624dab8740770d0d
ysamd1 <161851962+ysamd1@users.noreply.github.com>
2024-11-01 16:06:10 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+int main(void) 
+  
+   {
+    for (int i = 99; i >= 51; i -= 3) {
+        printf("%d", i);
+        if (i > 51) {
+            printf(", ");
+        }
+    }
+    return 0;
+}
