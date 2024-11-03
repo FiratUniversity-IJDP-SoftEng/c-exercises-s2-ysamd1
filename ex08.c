fee99e108bdb33e845eb3817083a0d5374641e38
ysamd1 <161851962+ysamd1@users.noreply.github.com>
2024-11-03 11:09:06 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,17 @@
 
+#include <stdio.h>
+
+int main() {
+    float num, sum = 0;
+
+    printf("Enter 10 numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &num);
+        sum += num;
+    }
+
+    printf("Sum: %.2f\n", sum);
+    printf("Average: %.2f\n", sum / 10);
+
+    return 0;
+}
